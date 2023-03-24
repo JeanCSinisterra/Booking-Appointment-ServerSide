063ac3e34d45481cff4179d466fa3e8d29fab350
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-03-24 18:35:46 -0500

    change in the doctor route for appointments

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 const Doctor = require("../models/doctorModel");
+const Appointment = require("../models/appointmentModel");
 
 // Route to render the layout of a Doctor Profile with the original information
 router.post("/get-doctor-info-by-user-id", authMiddleware,  async (req, res) => {
@@ -53,6 +54,25 @@ router.post("/get-doctor-info-by-id", authMiddleware, async (req, res) => {
         res
             .status(500)
             .send({ message: "Error getting Doctor info", success: false, error });
+    }
+})
+
+// Route to render the layout of the Appointments
+router.get("/get-appointments-by-doctor-id", authMiddleware, async (req, res) => {
+    try {
+        const doctor = await Doctor.findOne({ userId: req.body.userId });
+        const appointments = await Appointment.findOne({ doctorId: doctor._id})
+        res.status(200)
+            .send({
+                success: true,
+                message: "Appointments fetched successfully",
+                data: appointments
+            });
+    } catch (error) {
+        console.log(error);
+        res
+            .status(500)
+            .send({ message: "Error fetching Appointments", success: false, error });
     }
 })
 
