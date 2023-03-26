7851d2376ed4219cf82cc404c61e9829cd9350da
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-03-25 23:55:34 -0500

    changes on the Appointments status

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -3,11 +3,12 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 const Doctor = require("../models/doctorModel");
 const Appointment = require("../models/appointmentModel");
+const User = require("../models/userModel");
 
 // Route to render the layout of a Doctor Profile with the original information
 router.post("/get-doctor-info-by-user-id", authMiddleware,  async (req, res) => {
     try {
-        const doctor = await Doctor.findOne({ userId: req.body.user._id });
+        const doctor = await Doctor.findOne({ userId: req.body.userId });
         res.status(200)
             .send({ 
                 success: true, 
@@ -71,6 +72,34 @@ router.get("/get-appointments-by-doctor-id", authMiddleware, async (req, res) =>
     } catch (error) {
         console.log(error);
         res.status(500).send({ message: "Error fetching Appointments", success: false, error });
+    }
+})
+
+// Route to Approve Status for Doctor Apply
+router.post("/change-appointment-status", authMiddleware, async (req, res) => {
+    try {
+        const { appointmentId, status } = req.body;
+        const appointment = await Appointment.findByIdAndUpdate(appointmentId, {
+            status,
+        });
+        // find first the user
+        const user = await User.findOne({ _id: appointment.userId });
+        // send the notifications to the user
+        const unseenNotifications = user.unseenNotifications;
+        unseenNotifications.push({
+            type: "appointment-status-changed",
+            message: `Your appointment account has been ${status}`,
+            onClickPath: "/appointments"
+        });
+        await user.save()
+
+        res.status(200).send({
+            message: "Appointment status updated successfully",
+            success: true,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "Error changing Appointment status", success: false, error });
     }
 })
 
