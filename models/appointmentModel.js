79e2a567bf71685a5371b684100a70250df8424e
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-03-26 13:59:08 -0500

    changes to debug and test

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -27,7 +27,7 @@ const appointmentSchema = new mongoose.Schema({
     status: {
         type: String,
         required: true,
-        default: "pending"
+        default: "Pending"
     }
 }, 
     {timestamps: true}
