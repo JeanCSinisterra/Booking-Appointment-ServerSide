a9cb0c17d87add6f47af0731a3baa68419f843f6
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-03-26 14:26:41 -0500

    small changes

--- routes/doctorRoute.js
+++ routes/doctorRoute.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
+const User = require("../models/userModel");
 const Doctor = require("../models/doctorModel");
 const Appointment = require("../models/appointmentModel");
-const User = require("../models/userModel");
+
 
 // Route to render the layout of a Doctor Profile with the original information
 router.post("/get-doctor-info-by-user-id", authMiddleware,  async (req, res) => {
     try {
-        const doctor = await Doctor.findOne({ userId: req.body.userId });
+        const doctor = await Doctor.findOne({ _id: req.body.userId});
         res.status(200)
             .send({ 
                 success: true, 
                 message: "Doctor info fetched successfully", 
                 data: doctor });
     } catch (error) {
-        console. log(error);
+        console.log(error);
         res
             .status(500)
             .send({ message: "Error getting Doctor info", success: false, error });
@@ -25,7 +26,7 @@ router.post("/get-doctor-info-by-user-id", authMiddleware,  async (req, res) => 
 // Route to get updates in the Doctor Profile
 router.post("/update-doctor-profile", authMiddleware, async (req, res) => {
     try {
-        const doctor = await Doctor.findOneAndUpdate({ userId: req.body.userId }, req.body);
+        const doctor = await Doctor.findOneAndUpdate({ _id: req.body.userId }, req.body);
         res.status(200)
             .send({
                 success: true,
